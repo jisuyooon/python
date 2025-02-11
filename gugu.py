b14318e07efd243e6ce9066c31e6bd9368aa81b3
jisuyooon <tnwl212@naver.com>
2025-02-11 17:15:46 +0900

    11일

--- gugu.py
+++ gugu.py
@@ -1,13 +1,11 @@
 import sys
+args = sys.argv
 
-def gugu(end = 9):
-    data =''
-    for j in range(2,end + 1):
-        print(f"{str(j)+'단':=^10}")
-        for i in range(1,10):
-            data += (f'{j} x {i} = {j*i}')+'\n'
-    return data
-print(gugu(2))
-
-result = int() + int()
-print(result)
+aa, bb = int(args[1][:-1]), int(args[2])
+print(aa,type(aa),bb,type(bb))
+def gugu(a,b): 
+    for i in range(a,a+b):
+        print(f"==={i}단===")
+        for j in range(1,10):
+            print(f"{i}x{j}={i*j}")
+gugu(aa,bb)
