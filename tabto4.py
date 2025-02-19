46d02d8929aeacff7653dd1aee5e82a389086bb7
jisuyooon <tnwl212@naver.com>
2025-02-19 16:32:52 +0900

    haha

--- tabto4.py
+++ tabto4.py
@@ -3,5 +3,23 @@ import sys
 src = sys.argv[1]
 dst = sys.argv[2]
 
-print(src)
-print(dst)
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace("\t", " " * 4)
+print(space_content)
+# import sys
+
+# src = sys.argv[1]
+# dst = sys.argv[2]
+
+# f = open(src)
+# tab_content = f.read()
+# f.close()
+
+# space_content = tab_content.replace("\t", " " * 4)
+
+# f = open(dst, 'w')
+# f.write(space_content)
+# f.close()
