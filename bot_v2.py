9cd6292a66291be293bde26f92bbca72ed914096
jisuyooon <tnwl212@naver.com>
2025-02-25 14:54:38 +0900

    최최신

--- bot_v2.py
+++ bot_v2.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import talk_yjs as tk
+import gemini
 
 TOKEN = ''
 
@@ -12,15 +13,27 @@ TOKEN = ''
 
 async def start(update, context):
     await update.message.reply_text("안녕하세요! 저는 샘봇 입니다. 무엇을 도와드릴까요?")
+
+async def send_photo(update, context):
+    photo_url = "https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp"
+    await update.message.reply_photo(photo=photo_url,caption="사진 이미지 불러왔어요~")
     
 async def monitor_chat(update, context):
     user_text = update.message.text # 감지된 메시지들 # 택배 알맹이
     chat_id = update.message.chat_id # 메시지가 온 채팅방 # 택배 주소지  
 
-    for key, res in tk.TRIGGER_WORDS.items():
-        if key in user_text:
-            await context.bot.send_message(chat_id=chat_id, text=res)
-            break # 한개의 키워드에만 반응
+    if "gpt" in user_text:
+        res = aiai(user_text.replace("gpt ",""))
+        await context.bot.send_message(chat_id=chat_id, text=res)
+    elif "영화정보" in user_text: pass
+        # await 영화정보크롤링()함수를 실행
+    elif "사진줘" in user_text:
+        send_photo()
+    else:
+        for key, res in tk.TRIGGER_WORDS.items():
+            if key in user_text:
+                await context.bot.send_message(chat_id=chat_id, text=res)
+                break # 한개의 키워드에만 반응
     
 def main():
     app = Application.builder().token(TOKEN).build()
